61bd9e34d866b4944d087a60e7b318bec2e7bd2b
chris <your.email@domain.com>
2018-02-25 18:27:20 -0400

    Done with the first exercise.

---
+++ data/src/data/Area.java
@@ -0,0 +1,16 @@
+package data;
+
+public class Area {
+	 public static void main(String[] args) {
+		 System.out.println("Data Structure");
+	 }
+	 public static double AreaC(int r){
+		 return 3.14*r*r;
+	 }
+	 public static double HalfAreaC(int r){
+		 return (3.14*r*r)/2;
+	 }
+	 public static double PerimeterC(int r){
+		 return 2*3.14*r;
+	 }
+}
